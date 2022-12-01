9631823c89c7ec9b32bc4dcd85c272634ffbe3cb
KinXpeng <kinxpeng@163.com>
2022-12-01 16:02:24 +0800

    feat:增加灰色背景

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
 <template>
-	<div id="app">
+	<div id="app" :class="momentSilence">
 		<router-view />
 	</div>
 </template>
 
-<style lang="less"></style>
+<script>
+	export default {
+		data() {
+			return {
+				momentSilence: '',
+				silenceList: ['4-4', '04-04', '11-01', '11-02', '11-03']
+			}
+		},
+		created() {
+			const date = new Date().Format('MM-dd')
+			if (this.silenceList.includes(date)) {
+				this.momentSilence = 'silence'
+			} else {
+				this.momentSilence = ''
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+	.silence {
+		-moz-filter: grayscale(100%);
+		-ms-filter: grayscale(100%);
+		-o-filter: grayscale(100%);
+		filter: grayscale(100%);
+		filter: gray;
+	}
+</style>
